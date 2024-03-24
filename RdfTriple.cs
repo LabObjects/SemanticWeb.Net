f17df2ee342b683ed74b7db4e4cd27d282e2d1c6
Lab Objects <LabObjects@users.noreply.github.com>
2024-03-24 13:39:22 -0400

    Add files via upload

---
+++ RdfTriple.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SemanticWeb.Rdf
+{
+    public class RdfTriple
+    {
+        public object Subject { get; set; }
+        public object Predicate { get; set; }
+        public object Object { get; set; }
+    }
+}
